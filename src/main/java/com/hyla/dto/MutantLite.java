7e608c46044c510c0560a25f14c0cad6c432037f
Sean Carlisle <s.m.carlisle@gmail.com>
2017-04-26 15:25:23 -0500

    add alive to mutant response list

--- src/main/java/com/hyla/dto/MutantLite.java
+++ src/main/java/com/hyla/dto/MutantLite.java
@@ -8,9 +8,11 @@ import lombok.Data;
 public class MutantLite {
     private Long id;
     private String name;
+    private boolean alive;
     
     MutantLite(Mutant m){
         this.id = m.getId();
         this.name = m.getName();
+        this.alive = m.isAlive();
     }
 }
